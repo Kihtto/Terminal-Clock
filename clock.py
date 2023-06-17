81c9996b010508bcc0451fc896e022441ea4180d
Clonne <kihtto@gmail.com>
2023-06-16 22:05:51 -0400

    add cron function

--- clock.py
+++ clock.py
@@ -43,20 +43,52 @@ def print_clock(text):
   for line in range(5):
     print_center(line, clock)
 
-def print_temp(key, init_time, actual_time, pause):
-  if pause == False:
+def print_temp(key, init_time, actual_time, clear_temp):
+  if clear_temp == False:
     delta_time = actual_time - init_time
     print_clock('0' +str(timedelta(seconds=delta_time)).split(sep='.')[0])
   else:
     print_clock('00:00:00')
   if key == "\n":
-    if pause:
+    if clear_temp:
       init_time = time.time()
-    pause = not pause
-  return init_time, pause
+    clear_temp = not clear_temp
+  return init_time, clear_temp
 
-def print_cron(key):
-  print_clock('00:00:00')
+def print_cron(key, chain, init_time_cron, actual_time, clear_cron,run):
+  chain_seconds = 0
+  if not clear_cron:
+    delta_time = actual_time - init_time_cron
+    if (int(chain[0:2])*60*60 + int(chain[2:4])*60 + int(chain[4:6])) > int(delta_time):
+      chain_seconds = timedelta(hours=int(chain[0:2]),minutes=int(chain[2:4]), seconds=int(chain[4:6])-delta_time+1)
+      run = True
+      if chain[0] != 0:
+        print_clock('0' +str(chain_seconds).split(sep='.')[0])
+      else:
+        print_clock('0' +str(chain_seconds).split(sep='.')[0])
+    else:
+      print_clock('00:00:00')
+      run = False
+      clear_cron = not clear_cron
+  else:
+    print_clock('00:00:00')
+  list_numbers = ['0','1','2','3','4','5','6','7','8','9']
+  if key in list_numbers and len(chain) < 6:
+    chain = chain + str(key)
+  if key == '\x7f' and len(chain) != 0 and not run:
+    chain = chain[:-1]
+  if key == "\n" and len(chain) == 6:
+    if clear_cron:
+      init_time_cron = time.time()
+    clear_cron = not clear_cron
+  if len(chain) > 4:
+    time_cron = chain[:2] + ":" + chain[2:4] + ":" + chain[4:]
+  elif len(chain) > 2:
+    time_cron = chain[:2] + ":" + chain[2:]
+  else:
+    time_cron = chain
+  print("time:" + time_cron)
+  return chain, init_time_cron,clear_cron
 
 def print_menu(resaltado):
   text = "Cron.  Temp.  Reloj"
@@ -76,7 +108,11 @@ with term.cbreak(), term.hidden_cursor():
   key = ''
   resaltado = 3
   init_time = ''
-  pause = True
+  clear_temp = True
+  chain = ''
+  init_time_cron = time.time()
+  clear_cron = True
+  run = False
   while key.lower() != 'q':
     key = term.inkey(timeout=0.2)
     if key == "\x1b[C" and resaltado < 3:
@@ -87,7 +123,7 @@ with term.cbreak(), term.hidden_cursor():
     if resaltado == 3:
       print_clock(time.strftime("%H:%M:%S"))
     elif resaltado == 2:
-      init_time, pause = print_temp(key, init_time, time.time(), pause)
+      init_time, clear_temp = print_temp(key, init_time, time.time(), clear_temp)
     elif resaltado == 1:
-      print_cron(key)
+      chain, init_time_cron,clear_cron = print_cron(key,chain, init_time_cron, time.time(), clear_cron, run)
     print_menu(resaltado)
